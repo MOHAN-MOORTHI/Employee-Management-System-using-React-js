4bd3118e005c96d8dd1c2c6d2c172b9d53a2caaf
MOHAN P <152972970+MOHAN-MOORTHI@users.noreply.github.com>
2024-03-07 14:22:16 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+
+import Dashboard from "./page/Dashboard";
+
+
+function App() {
+  return (
+    <>
+    <Dashboard/>
+    </>
+  
+  );
+}
+
+export default App;
